ad1fb0e3938c1d56ed6ce94e08d16b8db19acfec
gordongest <gordon.gest@gmail.com>
2021-05-20 12:13:41 -0600

    adding pipe to commit

---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert'
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: number, ...args: unknown[]): unknown {
+    if (!value) {
+      return 0;
+    }
+
+    return value * 1.60934;
+  }
+
+}
